8c635d90a299707e982afeb694a3f97858116db7
William Cantin <william@hugocantin.info>
2014-12-03 15:57:00 -0500

    Added entraineur_add.php :panda_face:

---
+++ JuliePro/html/entraineur_manager/entraineur_add.php
@@ -0,0 +1,96 @@
+<?php include '../view/header.php'; ?>
+
+    <!--  Section PROMO
+    ========================================== -->
+
+    <section>
+        <div class="wrapper">
+            <h2>Inscrire un Entraineur</h2>
+            <form class="grille_12">
+                <div class="grille_12">
+                    Nom:
+                    &nbsp;
+                    &nbsp;
+                    &nbsp;
+                    &nbsp;
+                    &nbsp;
+                    &nbsp;
+                    &nbsp;
+                    &nbsp;
+
+                    <input type="text" name="Nom">
+                    &nbsp;
+                    Prenom:
+                    &nbsp;
+                    &nbsp;
+                    &nbsp;
+                    &nbsp;
+                    &nbsp;
+                    <input type="text" name="Prenom">
+                    <br>
+                </div>
+                <div class="grille_12">
+                    No téléphone:
+                    <input type="text" name="Tel">
+                    &nbsp;
+                    No Cellulaire:
+                    <input type="text" name="Cell">
+                    <br>
+                </div>
+                <div class="grille_12">
+                    Adresse:
+                    &nbsp;
+                    &nbsp;
+                    &nbsp;
+                    &nbsp;
+                    &nbsp;
+                    <input type="text" name="Adresse">
+                    &nbsp;
+                    &nbsp;
+                    Ville:
+                    &nbsp;
+                    &nbsp;
+                    &nbsp;
+                    &nbsp;
+                    &nbsp;
+                    &nbsp;
+                    &nbsp;
+                    &nbsp;
+                    <input type="text" name="Ville">
+                    Code Postal:
+                    <input type="text" name="CodePostal">
+                    <br>
+                </div>
+                <div class="grille_12">
+                    Age:
+                    &nbsp;
+                    &nbsp;
+                    &nbsp;
+                    &nbsp;
+                    &nbsp;
+                    &nbsp;
+                    &nbsp;
+                    &nbsp;
+                    &nbsp;
+                    &nbsp;
+                    <input type="text" name="Age">
+                    <br>
+                </div>
+                <div class="grille_12">
+                    Date inscription:
+                    <input type="text" name="DateInsc">
+                    <br>
+                </div>
+                <div class="grille_12">
+                    Courriel:
+                    <input type="text" name="Courriel">
+                    <br>
+                </div>
+
+            </form>
+        </div>
+    </section>
+
+    <!--  FOOTER
+    ========================================== -->
+<?php include '../view/footer.php'; ?>
