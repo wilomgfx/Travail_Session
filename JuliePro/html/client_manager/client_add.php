6253c7b6cbbf259e92f6750a01a04144faea56b9
blocdeglace19952 <jonathanplamondon2@gmail.com>
2014-12-03 16:32:22 -0500

    Ajout form Consultation/Modification Info Client
    Ajout form Consultation/Modification Info Client

--- JuliePro/html/client_manager/client_add.php
+++ JuliePro/html/client_manager/client_add.php
@@ -1,85 +1,176 @@
 <?php include '../view/header.php'; ?>
 
+<!-- Section Consulter/Modifier Client -->
+<section>
+    <div class="wrapper">
+        <h2>Information Client</h2>
+        <form class ="grille_12">
+            <table>
+                 <tr>
+                     <td align="right">
+                           Client:
+                         <input list="lstClient" name="listePersonne">
+                         <datalist id="lstClient">
+                         <option value="Steven">
+                         </datalist>
+                     </td>
+                </tr>
+                <tr>
+                    <td align="right">
+                        Nom:
+                        <input type="text" name="Nom">
+                    </td>
+                    <td align="right">
+                        Prenom:
+                        <input type="text" name="Prenom">
+                    </td>
+                </tr>
+                <tr>
+                    <td align="right">
+                        No téléphone:
+                        <input type="text" name="Tel">
+                    </td>
+                    <td align="right">
+                        No Cellulaire:
+                        <input type="text" name="Cell">
+                    </td>
+                </tr>
+                <tr>
+                    <td align="right">
+                        Adresse:
+                        <input type="text" name="Adresse">
+                    </td>
+                    <td align="right">
+                        Ville:
+                        <input type="text" name="Ville">
+                    </td>
+                    <td align="right">
+                        Code Postal:
+                        <input type="text" name="CodePostal">
+                    </td>
+                </tr>
+
+                <tr>
+                    <td align="right">
+                        Age:
+                        <input type="number" name="Age">
+                    </td>
+                </tr>
+
+                <tr>
+                    <td align="right">
+                        Date inscription:
+                        <input type="date" name="DateInsc">
+                    </td>
+                </tr>
+
+                <tr>
+                    <td align="right">
+                        Courriel:
+                        <input type="email" name="Courriel">
+                    </td>
+                </tr>
+
+                <tr>
+                    <td align="right">
+                        Entraineur:
+                        <input type="text" name="Entraineur">
+                    </td>
+                </tr>
+                <tr>
+                    <td align="right">
+                        <input type="submit" value="Inscrire">
+                    </td>
+                </tr>
+
+            </table>
+        </form>
+    </div>
+</section>
+
+
+
+<!-- Section inscription Client -->
 <section>
     <div class="wrapper">
         <h2>Inscrire un Client</h2>
 
 
-    <form class ="grille_12">
-        <table>
-           <tr>
-                <td align="right">
-                     Nom:
-                     <input type="text" name="Nom">
-                  </td>
-                  <td align="right">
-                      Prenom:
-                     <input type="text" name="Prenom">
-                  </td>
-           </tr>
-           <tr>
-               <td align="right">
-                No téléphone:
-                 <input type="text" name="Tel">
-                 </td>
-               <td align="right">
-                 No Cellulaire:
-                <input type="text" name="Cell">
-                </td>
-            </tr>
-            <tr>
-                <td align="right">
-                     Adresse:
-                    <input type="text" name="Adresse">
-                </td>
-                <td align="right">
-                    Ville:
-                    <input type="text" name="Ville">
-                </td>
-                <td align="right">
-                     Code Postal:
-                     <input type="text" name="CodePostal">
-                 </td>
-            </tr>
+         <form class ="grille_12">
+            <table>
+                <tr>
+                    <td align="right">
+                          Nom:
+                          <input type="text" name="Nom">
+                     </td>
+                     <td align="right">
+                          Prenom:
+                         <input type="text" name="Prenom">
+                    </td>
+                 </tr>
+                 <tr>
+                     <td align="right">
+                        No téléphone:
+                        <input type="text" name="Tel">
+                     </td>
+                     <td align="right">
+                        No Cellulaire:
+                         <input type="text" name="Cell">
+                     </td>
+                 </tr>
+                 <tr>
+                     <td align="right">
+                          Adresse:
+                         <input type="text" name="Adresse">
+                     </td>
+                    <td align="right">
+                          Ville:
+                         <input type="text" name="Ville">
+                     </td>
+                      <td align="right">
+                          Code Postal:
+                         <input type="text" name="CodePostal">
+                      </td>
+                </tr>
 
-           <tr>
-               <td align="right">
-                 Age:
-                <input type="number" name="Age">
-               </td>
-            </tr>
+                 <tr>
+                      <td align="right">
+                         Age:
+                         <input type="number" name="Age">
+                    </td>
+                 </tr>
 
-            <tr>
-                <td align="right">
-                Date inscription:
-                <input type="date" name="DateInsc">
-                </td>
-            </tr>
+                  <tr>
+                     <td align="right">
+                        Date inscription:
+                         <input type="date" name="DateInsc">
+                      </td>
+                 </tr>
 
-             <tr>
-                 <td align="right">
-            Courriel:
-             <input type="email" name="Courriel">
-                 </td>
-             </tr>
+                 <tr>
+                      <td align="right">
+                          Courriel:
+                         <input type="email" name="Courriel">
+                       </td>
+                  </tr>
 
-            <tr>
-                <td align="right">
-             Entraineur:
-             <input type="text" name="Entraineur">
-                </td>
-            </tr>
-            <tr>
-                <td align="right">
-            <input type="submit" value="Inscrire">
-                </td>
-            </tr>
+                  <tr>
+                     <td align="right">
+                          Entraineur:
+                         <input type="text" name="Entraineur">
+                     </td>
+                  </tr>
+                  <tr>
+                     <td align="right">
+                   <input type="submit" value="Inscrire">
+                        </td>
+                  </tr>
 
-      </table>
-    </form>
+             </table>
+         </form>
 
 
-</div>
-    </section>
+    </div>
+</section>
 
 <?php include '../view/footer.php'; ?>
