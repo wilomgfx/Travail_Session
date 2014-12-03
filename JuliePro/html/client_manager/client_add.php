6c0707233c817b9758a74d0876081eddf26db976
blocdeglace19952 <jonathanplamondon2@gmail.com>
2014-12-03 15:34:32 -0500

    Formulaire Inscription Client
    Formulaire Inscription Client, manque css

---
+++ JuliePro/html/client_manager/client_add.php
@@ -0,0 +1,99 @@
+<?php include '../view/header.php'; ?>
+
+<div class="wrapper">
+
+    <h1 class="grille_12">Inscription Client</h1>
+
+    <form class ="grille_12">
+        <div class="grille_12">
+            Nom:
+            &nbsp;
+            &nbsp;
+            &nbsp;
+            &nbsp;
+            &nbsp;
+            &nbsp;
+            &nbsp;
+            &nbsp;
+
+            <input type="text" name="Nom">
+            &nbsp;
+             Prenom:
+            &nbsp;
+            &nbsp;
+            &nbsp;
+            &nbsp;
+            &nbsp;
+             <input type="text" name="Prenom">
+              <br>
+        </div>
+        <div class="grille_12">
+             No téléphone:
+            <input type="text" name="Tel">
+             &nbsp;
+             No Cellulaire:
+             <input type="text" name="Cell">
+             <br>
+         </div>
+        <div class="grille_12">
+            Adresse:
+            &nbsp;
+            &nbsp;
+            &nbsp;
+            &nbsp;
+            &nbsp;
+             <input type="text" name="Adresse">
+            &nbsp;
+            &nbsp;
+             Ville:
+            &nbsp;
+            &nbsp;
+            &nbsp;
+            &nbsp;
+            &nbsp;
+            &nbsp;
+            &nbsp;
+            &nbsp;
+            <input type="text" name="Ville">
+             Code Postal:
+             <input type="text" name="CodePostal">
+             <br>
+        </div>
+        <div class="grille_12">
+        Age:
+            &nbsp;
+            &nbsp;
+            &nbsp;
+            &nbsp;
+            &nbsp;
+            &nbsp;
+            &nbsp;
+            &nbsp;
+            &nbsp;
+            &nbsp;
+        <input type="text" name="Age">
+        <br>
+            </div>
+        <div class="grille_12">
+            Date inscription:
+            <input type="text" name="DateInsc">
+             <br>
+        </div>
+        <div class="grille_12">
+            Courriel:
+             <input type="text" name="Courriel">
+             <br>
+         </div>
+        <div class="grille_12">
+             Entraineur:
+             <input type="text" name="Entraineur">
+             <br>
+        </div>
+
+    </form>
+
+
+</div>
+
+
+<?php include '../view/footer.php'; ?>
---
+++ JuliePro/html/client_manager/index.php
@@ -0,0 +1,12 @@
+<?php
+//require('../model/database.php');
+//require('../model/client_db.php');
+
+    $action = 'client_add';
+
+if ($action == 'client_add') {
+    // Get the current category ID
+    // Display the product list
+    include('client_add.php');
+}
+?>
