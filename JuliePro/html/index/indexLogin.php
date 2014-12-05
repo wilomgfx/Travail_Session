4ed1bfffec48b2e0b89f336a81868bcd41b85457
Wilomgfx <william@hugocantin.info>
2014-12-05 12:08:56 -0500

    Ajout page login form et image JuliePro
    Created a simple juliePro logo on Photoshop and added a second index
    page for approval by the team

---
+++ JuliePro/html/index/indexLogin.php
@@ -0,0 +1,28 @@
+<head>
+<link href="../css/grille-base.css" rel="stylesheet">
+<link rel="stylesheet" href="../css/normalize.css">
+<link rel="stylesheet" href="../css/styleLogin.css">
+</head>
+<body>
+<!-- Login form -->
+</div>
+<section class="loginform cf">
+        <div class="wrapper">
+            <img src="../img/julieProLogoPetit.png">
+            <form name="login" action="index_submit" method="get" accept-charset="utf-8">
+                <ul>
+                    <li>
+                        <label for="user">Utilisateur</label>
+                        <input type="text" name="username" placeholder="yourusername" required>
+                    </li>
+                    <li>
+                        <label for="password">Password</label>
+                        <input type="password" name="password" placeholder="password" required></li>
+                    <li>
+                        <input type="submit" value="Se connecter">
+                    </li>
+                </ul>
+            </form>
+        </div>
+    </section>
+</body>
