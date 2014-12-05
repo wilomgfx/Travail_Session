ea3eb0237122627525983f54c08399a6d201793a
Wilomgfx <william@hugocantin.info>
2014-12-05 12:16:23 -0500

    index renamed

--- JuliePro/html/index/index.php
+++ JuliePro/html/index/index.php
@@ -1,115 +1,28 @@
-<?php include '../view/header.php'; ?>
-
-<!--  Section PROMO
-========================================== -->
-
-<section>
-    <div class="wrapper">
-        <figure id="promo_image" class="grille_12">
-            <img src="../../html/img/320.jpg" alt="Thor : un monde obscur">
-
-            <figcaption id="promo_caption">
-                <h1>« Entrez dans le monde de JuliePro »</h1>
-                <!--<a class="cta">Participer au concours</a>-->
-            </figcaption>
-        </figure>
-    </div>
-</section>
-
-<!--  Section INTRO
-========================================== -->
-    <section class="loginform cf">
+<head>
+<link href="../css/grille-base.css" rel="stylesheet">
+<link rel="stylesheet" href="../css/normalize.css">
+<link rel="stylesheet" href="../css/styleLogin.css">
+</head>
+<body>
+<!-- Login form -->
+</div>
+<section class="loginform cf">
         <div class="wrapper">
-        <form name="login" action="index_submit" method="get" accept-charset="utf-8">
-            <ul>
-                <li>
-                    <label for="user">Utilisateur</label>
-                    <input type="text" name="username" placeholder="yourusername" required>
-                </li>
-                <li>
-                    <label for="password">Password</label>
-                    <input type="password" name="password" placeholder="password" required></li>
-                <li>
-                    <input type="submit" value="Login">
-                </li>
-            </ul>
-        </form>
+            <img src="../img/julieProLogoPetit.png">
+            <form name="login" action="index_submit" method="get" accept-charset="utf-8">
+                <ul>
+                    <li>
+                        <label for="user">Utilisateur</label>
+                        <input type="text" name="username" placeholder="yourusername" required>
+                    </li>
+                    <li>
+                        <label for="password">Password</label>
+                        <input type="password" name="password" placeholder="password" required></li>
+                    <li>
+                        <input type="submit" value="Se connecter">
+                    </li>
+                </ul>
+            </form>
         </div>
     </section>
-
-<!--  Section HORAIRE
-========================================== -->
-<!--
-<section id="horaire">
-    <div class="wrapper">
-        <h2 class="grille_12"><span>Horaires</span> des films</h2>
-
-        <aside class="grille_6">
-            <h1>Recherche par films</h1>
-
-            <p>Choisissez un film selon son titre, sa date de sortie ou en fonction des cotes des critiques.</p>
-            <a class="cta" href="#">Trouver un film</a>
-        </aside>
-
-        <aside class="grille_6">
-            <h1>Recherche par cinémas</h1>
-
-            <p>Choisissez un cinémas selon son nom, sa localisation ou son offre de films.</p>
-            <a class="cta" href="#">Trouver un cinéma</a>
-        </aside>
-    </div>
-</section>
--->
-<!--  Section NOUVEAUTÉS
-========================================== -->
-<!--
-<section id="nouveaute">
-    <div class="wrapper">
-        <h2 class="grille_12"><span>Nouveautés</span> novembre 2013</h2>
-
-        <figure class="grille_3">
-            <img src="../../html/img/img-film-01.jpg" alt="Capitaine Phillips">
-
-            <figcaption>
-                <h3>Capitaine Phillips</h3>
-
-                <p><strong>De Paul Greengrass</strong><br>Avec Tom Hanks, Barkhad Abdi, Barkhad Abdirahman</p>
-            </figcaption>
-        </figure>
-
-        <figure class="grille_3">
-            <img src="../../html/img/img-film-02.jpg" alt="Vilain Grand-Père">
-
-            <figcaption>
-                <h3>Vilain Grand-Père</h3>
-
-                <p><strong>De Jeff Tremaine</strong><br>Avec Johnny Knoxville, Jackson Nicoll, Zia Harris</p>
-            </figcaption>
-        </figure>
-
-        <figure class="grille_3">
-            <img src="../../html/img/img-film-03.jpg" alt="La stratégie Ender">
-
-            <figcaption>
-                <h3>La stratégie Ender</h3>
-
-                <p><strong>De Gavin Hood</strong><br>Avec Harrison Ford, Asa Butterfield, Hailee Steinfeld</p>
-            </figcaption>
-        </figure>
-
-        <figure class="grille_3">
-            <img src="../../html/img/img-film-04.jpg" alt="Douze ans d’esclavage">
-
-            <figcaption>
-                <h3>Douze ans d’esclavage</h3>
-
-                <p><strong>De Steve McQueen</strong><br>Avec Chiwetel Ejiofor, Michael K. Williams, Michael Fassbender
-                </p>
-            </figcaption>
-        </figure>
-    </div>
-</section>
--->
-<!--  FOOTER
-========================================== -->
-<?php include '../view/footer.php'; ?>
+</body>
