2ad9e30a82c4c2095a2dab63f348f8f1122b9375
blocdeglace19952 <jonathanplamondon2@gmail.com>
2014-12-05 16:56:10 -0500

    Ajoute du fichier utilisateur_db
    Requête fournie en prime !

---
+++ JuliePro/html/model/utilisateur_db.php
@@ -0,0 +1,25 @@
+<?php
+
+function ajouter_utilisateur($Username, $Password, $Statut)
+{
+    global $db;
+    $query="INSERT INTO utilisateur (username, password, statut) VALUES('$Username', '$Password', '$Statut')";
+    $utilisateur = $db->query($query);
+    return $utilisateur;
+}
+function get_utilisateur_by_id($UtilisateurID)
+{
+    global $db;
+    $query ="Select * FROM utilisateur
+             where utilisateurID = '$UtilisateurID'";
+    $utilisateur = $db->query($query);
+    return $utilisateur;
+}
+function get_userID_by_username($Username)
+{
+    global $db;
+    $query ="Select * FROM utilisateur
+             where utilisateurID = '$Username'";
+    $utilisateur = $db->query($query);
+    return $utilisateur;
+}
