e3208d9c29ef180739be244ec5b503517b5e9411
blocdeglace19952 <jonathanplamondon2@gmail.com>
2014-12-05 16:55:12 -0500

    Ajout de 2 fichier(client_db et utilisateur_db)
    Requête fournie en prime ! Hell yeah !

--- JuliePro/html/model/client_db.php
+++ JuliePro/html/model/client_db.php
@@ -1,7 +1,35 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: 1250824
- * Date: 14-12-05
- * Time: 15:56
- */ 
+
+function get_client()
+{
+    global $db;
+    $query = 'SELECT * FROM client
+              ORDER BY clientID';
+    $client = $db->query($query);
+    return  $client;
+}
+function get_client_by_ID($clientID)
+{
+    global $db;
+    $query = "SELECT * FROM client
+              where clientID = '$clientID'";
+    $client = $db->query($query);
+    return  $client;
+}
+function inscrire_client($Nom, $Prenom, $NoTel, $NoCell, $Adresse, $Ville, $CodePostal, $Age, $DateInsc, $Courriel, $UtilisateurID,$EntraineurID)
+{
+    global $db;
+    $query = "INSERT INTO client (nom, prenom, numTel, numCell, adresse, ville, codePostal, age, dateInscription, courriel, FK_utilisateurID,FK_entraineurID) VALUES('$Nom', '$Prenom', '$NoTel', '$NoCell', '$Adresse', '$Ville', '$CodePostal', '$Age', '$DateInsc', '$Courriel','$UtilisateurID', '$EntraineurID')";
+    $client = $db->query($query);
+    return  $client;
+}
+function modifier_client($clientID, $Nom, $Prenom, $NoTel, $NoCell, $Adresse, $Ville, $CodePostal, $Age, $DateInsc, $Courriel, $UtilisateurID,$EntraineurID)
+{
+    global $db;
+    $query = "UPDATE client
+              SET nom ='$Nom', prenom ='$Prenom', numTel ='$NoTel', numCell='$NoCell', adresse='$Adresse', ville='$Ville', codePostal='$CodePostal', age='$Age', dateInscription='$DateInsc', courriel='$Courriel',FK_utilisateurID ='$UtilisateurID', FK_entraineurID='$EntraineurID'
+              WHERE clientID = '$clientID'";
+    $client = $db->query($query);
+    return  $client;
+}
+
