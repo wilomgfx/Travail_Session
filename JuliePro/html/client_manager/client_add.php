711a16a3ab79f93fd7e91026abdbf07fc1d47d27
Darder006 <joel_x34345@hotmail.com>
2014-12-11 08:36:17 -0500

    Tab dans le form client_add
    C'est maintenant un peu moins dégueulasse.

--- JuliePro/html/client_manager/client_add.php
+++ JuliePro/html/client_manager/client_add.php
@@ -28,76 +28,76 @@
                     </table>
             </form>
             <?php if (isset($client)) : ?>
-            <form class ="grille_12" action="index.php"  method="post">
-                <!-- Pour avoir le id de l'entraineur -->
-                <input type="hidden" value="<?php echo $client['clientID'] ;?>" name="clientID">
+                <form class ="grille_12" action="index.php"  method="post">
+                    <!-- Pour avoir le id de l'entraineur -->
+                    <input type="hidden" value="<?php echo $client['clientID'] ;?>" name="clientID">
 
-            <input type="hidden" value="Modifier" name="action">
-            <table>
-                <tr>
-                    <td align="right">
-                        Nom:
-                        <input type="text" name="Nom" value="<?php echo $client['nom'] ;?>">
-                    </td>
-                    <td align="right">
-                        Prenom:
-                        <input type="text" name="Prenom" value="<?php echo $client['prenom'] ;?>" />
-                    </td>
-                </tr>
-                <tr>
-                    <td align="right">
-                        No téléphone:
-                        <input type="text" name="Tel" value="<?php echo $client['numTel'] ;?>" />
-                    </td>
-                    <td align="right">
-                        No Cellulaire:
-                        <input type="text" name="Cell" value="<?php echo $client['numCell'] ;?>" />
-                    </td>
-                </tr>
-                <tr>
-                    <td align="right">
-                        Adresse:
-                        <input type="text" name="Adresse" value="<?php echo $client['adresse'] ;?>" />
-                    </td>
-                    <td align="right">
-                        Ville:
-                        <input type="text" name="Ville" value="<?php echo $client['ville'] ;?>" />
-                    </td>
-                    <td align="right">
-                        Code Postal:
-                        <input type="text" name="CodePostal" value="<?php echo $client['codePostal'] ;?>" />
-                    </td>
-                </tr>
+                <input type="hidden" value="Modifier" name="action">
+                <table>
+                    <tr>
+                        <td align="right">
+                            Nom:
+                            <input type="text" name="Nom" value="<?php echo $client['nom'] ;?>">
+                        </td>
+                        <td align="right">
+                            Prenom:
+                            <input type="text" name="Prenom" value="<?php echo $client['prenom'] ;?>" />
+                        </td>
+                    </tr>
+                    <tr>
+                        <td align="right">
+                            No téléphone:
+                            <input type="text" name="Tel" value="<?php echo $client['numTel'] ;?>" />
+                        </td>
+                        <td align="right">
+                            No Cellulaire:
+                            <input type="text" name="Cell" value="<?php echo $client['numCell'] ;?>" />
+                        </td>
+                    </tr>
+                    <tr>
+                        <td align="right">
+                            Adresse:
+                            <input type="text" name="Adresse" value="<?php echo $client['adresse'] ;?>" />
+                        </td>
+                        <td align="right">
+                            Ville:
+                            <input type="text" name="Ville" value="<?php echo $client['ville'] ;?>" />
+                        </td>
+                        <td align="right">
+                            Code Postal:
+                            <input type="text" name="CodePostal" value="<?php echo $client['codePostal'] ;?>" />
+                        </td>
+                    </tr>
 
-                <tr>
-                    <td align="right">
-                        Age:
-                        <input type="number" name="Age" value="<?php echo $client['age'] ;?>" />
-                    </td>
-                </tr>
+                    <tr>
+                        <td align="right">
+                            Age:
+                            <input type="number" name="Age" value="<?php echo $client['age'] ;?>" />
+                        </td>
+                    </tr>
 
-                <tr>
-                    <td align="right">
-                        Date inscription:
-                        <input type="text" name="DateInsc" value="<?php echo $client['dateInscription'] ;?>" />
+                    <tr>
+                        <td align="right">
+                            Date inscription:
+                            <input type="text" name="DateInsc" value="<?php echo $client['dateInscription'] ;?>" />
 
-                    </td>
-                </tr>
+                        </td>
+                    </tr>
 
-                <tr>
-                    <td align="right">
-                        Courriel:
-                        <input type="email" name="Courriel" value="<?php echo $client['courriel'] ;?>" />
-                    </td>
-                </tr>
-                <tr>
-                    <td align="right">
-                        <input type="submit" value="Modifier">
-                    </td>
-                </tr>
+                    <tr>
+                        <td align="right">
+                            Courriel:
+                            <input type="email" name="Courriel" value="<?php echo $client['courriel'] ;?>" />
+                        </td>
+                    </tr>
+                    <tr>
+                        <td align="right">
+                            <input type="submit" value="Modifier">
+                        </td>
+                    </tr>
 
-            </table>
-        </form>
+                </table>
+            </form>
         <?php endif; ?>
     </div>
 </section>
